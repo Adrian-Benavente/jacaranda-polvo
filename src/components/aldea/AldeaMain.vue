6e1d414a4c11c621a58214313dc67298d9de1a37
benaventeadrian <benaventeadrian@gmail.com>
2020-10-31 16:29:05 -0300

    feat(parallax): make parallax independet for every item

--- src/components/aldea/AldeaMain.vue
+++ src/components/aldea/AldeaMain.vue
@@ -8,58 +8,52 @@
         aria-hidden="true"
         class="aldea__background"
       >
-        <video
-          src="../../assets/video/aldea-background.mp4"
-          autoplay
-          loop
-          muted
-        ></video>
       </kinesis-element>
       <kinesis-element
-        type="depth"
+        type="depth_inv"
         :strength="5"
         class="aldea__pyramid"
         aria-hidden="true"
       ></kinesis-element>
-      <kinesis-element type="depth_inv" tag="nav">
+      <nav>
         <ul>
-          <li>
-            <a class="aldea__item tv" href="#tv">
+          <li class="aldea__item tv">
+            <kinesis-element :strength="30" tag="a" href="#tv">
               <span class="aldea__item-text">Tv</span>
-            </a>
+            </kinesis-element>
           </li>
-          <li>
-            <a class="aldea__item teatro" href="#teatro"
-              ><span class="aldea__item-text">Teatro</span></a
+          <li class="aldea__item teatro">
+            <kinesis-element :strength="5" tag="a" href="#teatro"
+              ><span class="aldea__item-text">Teatro</span></kinesis-element
             >
           </li>
-          <li>
-            <a class="aldea__item contenido" href="#contenido"
-              ><span class="aldea__item-text">Contenido</span></a
+          <li class="aldea__item contenido">
+            <kinesis-element :strength="30" tag="a" href="#contenido"
+              ><span class="aldea__item-text">Contenido</span></kinesis-element
             >
           </li>
-          <li>
-            <a class="aldea__item albums" href="#albums"
-              ><span class="aldea__item-text">Albums</span></a
+          <li class="aldea__item albums">
+            <kinesis-element :strength="80" tag="a" href="#albums"
+              ><span class="aldea__item-text">Albums</span></kinesis-element
             >
           </li>
-          <li>
-            <a class="aldea__item contacto" href="#contacto"
-              ><span class="aldea__item-text">Contacto</span></a
+          <li class="aldea__item contacto">
+            <kinesis-element tag="a" href="#contacto"
+              ><span class="aldea__item-text">Contacto</span></kinesis-element
             >
           </li>
-          <li>
-            <a class="aldea__item sesiones" href="#sesiones"
-              ><span class="aldea__item-text">Sesiones</span></a
+          <li class="aldea__item sesiones">
+            <kinesis-element :strenth="50" tag="a" href="#sesiones"
+              ><span class="aldea__item-text">Sesiones</span></kinesis-element
             >
           </li>
-          <li>
-            <a class="aldea__item cine" href="#cine"
-              ><span class="aldea__item-text">Cine</span></a
+          <li class="aldea__item cine">
+            <kinesis-element :strength="70" tag="a" href="#cine"
+              ><span class="aldea__item-text">Cine</span></kinesis-element
             >
           </li>
         </ul>
-      </kinesis-element>
+      </nav>
     </kinesis-container>
   </section>
 </template>
@@ -108,56 +102,97 @@ export default {
     background-repeat: no-repeat;
     background-size: contain;
     display: block;
-    height: 100%;
     text-decoration: none;
-    width: 100%;
-    &-text {
-      opacity: 0;
+    a {
+      @extend %middle;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: contain;
+      display: block;
+      height: 100%;
+      text-decoration: none;
+      width: 100%;
     }
-    &:hover &-text {
-      font-family: "bebas_neuebold", arial, sans-serif;
-      font-size: 1.25rem;
+    &-text {
+      @extend %middle;
       color: white;
-      opacity: 1;
-      position: relative;
+      font-family: "bebas_neuebold", arial, sans-serif;
+      font-size: 1rem;
       text-align: center;
       text-transform: uppercase;
-      top: 105%;
+      top: 100%
+    }
+    @media (min-width: 800px) {
+      &-text {
+        opacity: 0;
+      }
+      &:hover &-text {
+        font-size: 1.25rem;
+        opacity: 1;
+      }
     }
     &.tv {
-      background-image: url("../../assets/img/aldea/tv.svg");
-      height: fn.to-proportion-width(107.96, 1440);
-      width: fn.to-proportion-width(63.34, 1440);
+      min-height: 64.776px;
+      min-width: 38.004px;
+      height: fn.to-proportion-width(117.96, 1440);
+      width: fn.to-proportion-width(73.34, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/tv.svg");
+      }
     }
     &.teatro {
-      background-image: url("../../assets/img/aldea/teatro.svg");
-      height: fn.to-proportion-width(117.61, 1440);
-      width: fn.to-proportion-width(169.88, 1440);
+      min-height: 70.566px;
+      min-width: 101.928px;
+      height: fn.to-proportion-width(87.61, 1440);
+      width: fn.to-proportion-width(139.88, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/teatro.svg");
+      }
     }
     &.contenido {
-      background-image: url("../../assets/img/aldea/contenido.svg");
+      min-height: 53.556px;
+      min-width: 82.2px;
       height: fn.to-proportion-width(89.26, 1440);
       width: fn.to-proportion-width(137, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/contenido.svg");
+      }
     }
     &.albums {
-      background-image: url("../../assets/img/aldea/albums.svg");
-      height: fn.to-proportion-width(116.34, 1440);
-      width: fn.to-proportion-width(99, 1440);
+      min-height: 69.804px;
+      min-width: 59.4px;
+      height: fn.to-proportion-width(166.34, 1440);
+      width: fn.to-proportion-width(149, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/albums.svg");
+      }
     }
     &.contacto {
-      background-image: url("../../assets/img/aldea/contacto.svg");
-      height: fn.to-proportion-width(90, 1440);
-      width: fn.to-proportion-width(150, 1440);
+      min-height: 54px;
+      min-width: 90px;
+      height: fn.to-proportion-width(60, 1440);
+      width: fn.to-proportion-width(130, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/contacto.svg");
+      }
     }
     &.sesiones {
-      background-image: url("../../assets/img/aldea/sesiones.svg");
-      height: fn.to-proportion-width(117.39, 1440);
-      width: fn.to-proportion-width(94.89, 1440);
+      min-height: 70.434px;
+      min-width: 56.934px;
+      height: fn.to-proportion-width(107.39, 1440);
+      width: fn.to-proportion-width(84.89, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/sesiones.svg");
+      }
     }
     &.cine {
-      background-image: url("../../assets/img/aldea/cine.svg");
-      height: fn.to-proportion-width(109, 1440);
-      width: fn.to-proportion-width(108.99, 1440);
+      min-height: 65.4px;
+      min-width: 65.394px;
+      height: fn.to-proportion-width(129, 1440);
+      width: fn.to-proportion-width(128.99, 1440);
+      a {
+        background-image: url("../../assets/img/aldea/cine.svg");
+      }
     }
   }
   &__menu,
@@ -171,54 +206,79 @@ export default {
       display: flex;
       list-style: none;
       position: relative;
-
-      li {
-        background-color: peru;
-      }
     }
     &.explode .aldea__item {
       opacity: 1;
       transform: scale(1);
+      //&:hover {
+      //  transform: scale(1.1);
+      //}
     }
     &.explode .tv {
-      top: -65%;
+      top: -66%;
+      left: -45%;
+      @media (min-width: 800px) {
+        top: -65%;
+        left: 0;
+      }
     }
     &.explode .teatro {
-      top: -45%;
+      top: -60%;
       left: 30%;
+      @media (min-width: 800px) {
+        top: -45%;
+        left: 30%;
+      }
     }
     &.explode .contenido {
-      top: 10%;
-      left: 55%;
+      top: -35%;
+      left: 50%;
+      @media (min-width: 800px) {
+        top: 10%;
+        left: 55%;
+      }
     }
     &.explode .albums {
-      top: 65%;
-      left: 30%;
+      top: 10%;
+      left: 20%;
+      @media (min-width: 800px) {
+        top: 65%;
+        left: 30%;
+      }
     }
     &.explode .contacto {
       top: 65%;
-      left: -25%;
+      @media (min-width: 800px) {
+        top: 65%;
+        left: -25%;
+      }
     }
     &.explode .sesiones {
-      top: 10%;
-      left: -55%;
+      top: 32%;
+      left: -60%;
+      @media (min-width: 800px) {
+        top: 10%;
+        left: -55%;
+      }
     }
     &.explode .cine {
-      top: -45%;
-      left: -30%;
+      top: -48%;
+      left: -100%;
+      @media (min-width: 800px) {
+        top: -45%;
+        left: -30%;
+      }
     }
   }
   &__background {
-    filter: brightness(70%);
+    background-image: url("../../assets/img/aldea/sky-bg.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter: brightness(50%) grayscale(100%);
     position: absolute;
     height: 150%;
     width: 150%;
-    video {
-      display: block;
-      height: 100%;
-      object-fit: cover;
-      width: 100%;
-    }
   }
   &__pyramid {
     @extend %middle;
@@ -227,6 +287,8 @@ export default {
     background-repeat: no-repeat;
     background-size: contain;
     height: fn.to-proportion-width(147.54, 1440);
+    min-height: 108px;
+    min-width: 300px;
     opacity: 0;
     transition: opacity 1s linear, transform 1s cubic-bezier(0.23, 1, 0.32, 1) !important;
     width: fn.to-proportion-width(440.72, 1440);
