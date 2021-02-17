26299b292968853f37664fbf3c17a338a86d8c70
benaventeadrian <benaventeadrian@gmail.com>
2021-02-16 22:57:21 -0300

    WIP: basic movie template

---
+++ src/components/cine/fichas/FichaCine.vue
@@ -0,0 +1,24 @@
+<template>
+  <section :class="$style.container">
+    <header :class="$style.header">
+      <h2 :class="$style.title">Lorem title</h2>
+      <p :class="$style.year">2017</p>
+    </header>
+    <main>
+      <section :class="$style.video"></section>
+      <section :class="$style.info">
+        <div :class="$style.about"></div>
+        <div :class="$style.poster"></div>
+        <div :class="$style.ficha"></div>
+      </section>
+    </main>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FichaCine",
+};
+</script>
+
+<style lang="scss" module></style>
