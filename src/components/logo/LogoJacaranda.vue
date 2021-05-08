3e0676be41b1888f4e12c4e3b6f42ef20651ec74
benaventeadrian <benaventeadrian@gmail.com>
2021-05-08 01:32:20 -0300

    hide logo when reaching "cine" to avoid overlap with the titles

--- src/components/logo/LogoJacaranda.vue
+++ src/components/logo/LogoJacaranda.vue
@@ -3,7 +3,7 @@
     <keep-alive>
       <router-link to="/#aldea" v-slot="{ href }">
         <a
-          :class="[$style.logo, webp && $style.webp]"
+          :class="[$style.logo, webp && $style.webp, hide && $style.hide]"
           :href="href"
           @click="$router.push(href)"
         >
@@ -21,6 +21,7 @@ export default {
   name: "LogoJacaranda",
   data() {
     return {
+      hide: false,
       wrap: false,
       webp: false,
     };
@@ -28,7 +29,11 @@ export default {
   mounted() {
     this.webp = document.documentElement.classList.contains("webp");
     window.addEventListener("scroll", () => {
-      this.wrap = window.pageYOffset >= window.innerHeight;
+      const isHome = this.$route.name === "home";
+      this.wrap = isHome && window.scrollY >= window.innerHeight;
+      this.hide =
+        isHome &&
+        window.pageYOffset >= window.innerHeight + window.innerHeight / 2;
     });
   },
 };
@@ -45,13 +50,12 @@ export default {
   font-size: 1rem;
   margin-left: fn.to-proportion-width(36, 1440);
   margin-top: fn.to-proportion-height(18, 900);
+  opacity: 1;
   overflow: hidden;
   position: relative;
   text-align: start;
+  transition: opacity 0.5s ease-in-out, margin-left 0.7s ease-in-out;
   z-index: 10001;
-  @media (min-width: 768px) {
-    font-size: fn.to-proportion-width(14, 1440);
-  }
   &::before {
     background-image: url("../../assets/img/logo/arbol.png");
     background-repeat: no-repeat;
@@ -62,6 +66,13 @@ export default {
     min-height: 62px;
     min-width: 62px;
     width: fn.to-proportion-width(62, 1440);
+  }
+  &.hide {
+    margin-left: -200px;
+    opacity: 0;
+  }
+  @media (min-width: 768px) {
+    font-size: fn.to-proportion-width(14, 1440);
   }
 }
 .text {
